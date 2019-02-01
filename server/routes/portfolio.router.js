2ab2374b27e3d9654fd0ecbbc951d09d259949a3
Waldyn Benbenek <wben1201@gmail.com>
2019-02-01 16:37:19 -0600

    Database created. Trello done. working on GET and POST serverside.

---
+++ server/routes/portfolio.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router
+router.get('/', (req, res) => {
+
+    res.sendStatus(200);
+
+    const queryText = `SELECT * FROM "projects"
+    JOIN "tags" ON "tags"."id" = "projects"".tag_id" ORDER BY "id" ASC`;
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error on query ${error}`);
+            res.sendStatus(500);
+        });
+
+});
+router.post('/', (req, res) => {
+
+    console.log(req.body)
+    let proj = req.body
+    const queryText = `INSERT INTO "projects" ( "name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
+                       VALUES ($1, $2, $3, $4, $5)`
+    pool.query(queryText, [proj.name, proj.description, proj.thumbnail, proj.website, proj.github, proj.date_completed, proj.tag_id])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error on query ${error}`);
+            res.sendStatus(500);
+        });
+});
--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-
+const portfolioRouter = 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/portfolio', portfolioRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
