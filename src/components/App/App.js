edd389f851013c4e15dd79dae086cd93bc4c6434
Waldyn Benbenek <wben1201@gmail.com>
2019-02-02 10:38:25 -0600

    I'm getting some work done on the job because it's slow today. Dropdown exists, but I have to think of a beeter way to get the values from it.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import adminForm from './../admin/adminForm';
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
+      <Router>
+        <Route exact path="/admin" component={adminForm} />
+        </Router>
         <p>Empty Page</p>
       </div>
     );
--- src/components/admin/adminForm.js
+++ src/components/admin/adminForm.js
@@ -1,6 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Dropdown from 'react-dropdown'
-
-class OrderForm extends Component {
+import 'react-dropdown/style.css'
+const options = [{ tag: 'react', tag_id: 1 }, { tag: 'jQuery', tag_id: 2 }, { tag: 'Node', tag_id: 3 }, { tag: 'sql', tag_id: 4 }, { tag: 'Redux', tag_id: 5 }, { tag: 'HTML', tag_id: 6 },]
+const defaultOption = options[0]
+    
+class AdminForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -18,17 +23,21 @@ class OrderForm extends Component {
     }
 render(){
     return (
-
+       
 <form onSubmit={this.addCustomer}>
     <input onChange={this.changeThumbnail} type="image" placeholder="thumbnail" />
     <input onChange={this.changeName} type="text" placeholder="thumbnail" />
     <input onChange={this.changeWebsite} type="text" placeholder="website" />
     <input onChange={this.changeGithub} type="text" placeholder="github" />
     <input onChange={this.changeDate_completed} type="number" placeholder="date" /> <br />
-    <input onChange={this.changeDescription} type="text" placeholder="Description"/>
-
+    <input onChange={this.changeDescription} type="text" placeholder="Description"/> <br></br>
+            < Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
    
     <input type="submit" value="Next" />
 
 </form>
     )}}
+const mapReduxStoreToProps = (reduxStore) => ({
+    reduxStore: reduxStore
+})
+export default connect(mapReduxStoreToProps)(AdminForm);
